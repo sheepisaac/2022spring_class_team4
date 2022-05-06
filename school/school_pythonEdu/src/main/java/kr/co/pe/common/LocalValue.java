c3b383711e7ca0b5c98d37a408084aa458e3bf05
CGE-Hub <59642557+CGE-Hub@users.noreply.github.com>
2022-05-06 19:08:27 +0900

    Update LocalValue.java

--- school/school_pythonEdu/src/main/java/kr/co/pe/common/LocalValue.java
+++ school/school_pythonEdu/src/main/java/kr/co/pe/common/LocalValue.java
@@ -2,14 +2,14 @@ package kr.co.pe.common;
 
 public class LocalValue {
 	
-	public final static String DB_IP = "localhost:3306";
-	public final static String DB_NAME = "school_pythonedu";
-	public final static String DB_ID = "school_pythonedu";
-	public final static String DB_PWD = "1234";
+	public final static String DB_IP = "";
+	public final static String DB_NAME = "";
+	public final static String DB_ID = "";
+	public final static String DB_PWD = "";
 
 	
-	public final String FILEUPLOAD_ROOT_PATH = "/resources/file";
-	public final String PYTHON_PATH = "C:/ProgramData/Anaconda3/envs/school_pythonedu/python.exe";
+	public final String FILEUPLOAD_ROOT_PATH = "";
+	public final String PYTHON_PATH = "";
 	
 	/* Google smtp 메일 ID와 패스워드 */
 	public final String G_MAIL_ID = "";
@@ -20,8 +20,8 @@ public class LocalValue {
 	
 	
 	/* cafe24 smtp 메일 ID와 패스워드 */
-	public final String SMTP_SERVER = "smpt.cafe24.com";
-	public final String SMTP_PORT = "587";
+	public final String SMTP_SERVER = "";
+	public final String SMTP_PORT = "";
 	public final String MAIL_ID = "";
 	public final String MAIL_PWD = "";
 	public final String FROM_ADDRESS = "";
